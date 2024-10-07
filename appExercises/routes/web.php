eacc94660f0fd7a124f8116092677a9ced8b7522
Myrella Almeida <myrella.alvesbcn@outlook.com>
2024-10-07 23:42:05 +0200

    cambio de rutas

--- appExercises/routes/web.php
+++ appExercises/routes/web.php
@@ -1,20 +1,7 @@
 <?php
 
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\ExercisesController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('appExercises')->group(function() {
-
-    Route::post('/user', [UserController::class, 'store']);
-    Route::put('/users/{id}', [UserController::class, 'update']);
-    Route::get('/users/{id}', [UserController::class, 'show']);
-    Route::delete('/users/{id}', [UserController::class, 'destroy']);
-
-    Route::post('/exercises', [ExercisesController::class, 'store']);
-    Route::get('/exercises', [ExercisesController::class, 'index']);
-    Route::post('/exercises/{id}/join', [ExercisesController::class, 'joinExercises']);
-
-    Route::get('/exercises/export', [ExercisesController::class, 'export']);
-    Route::post('/exercises/import', [ExercisesController::class, 'import']);
+Route::get('/', function () {
+    return view('welcome');
 });
