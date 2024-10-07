ab330dcfa9308015791031e6d740924a2d44c6a3
Myrella Almeida <myrella.alvesbcn@outlook.com>
2024-10-08 01:16:05 +0200

    ajustes modelos y rutas

--- appExercises/routes/api.php
+++ appExercises/routes/api.php
@@ -4,6 +4,8 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\ExercisesController;
 use Illuminate\Support\Facades\Route;
 
+
+
 Route::prefix('appExercises')->group(function() {
 
     Route::post('/user', [UserController::class, 'store']);
